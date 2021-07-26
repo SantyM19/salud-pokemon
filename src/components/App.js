b510dd1acd52167ce5e4afe1d2b3e6e327d5bfe2
Santiago Mejia <santiagomejiad19@hotmail.com>
2021-07-26 13:29:42 -0500

    Home

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Layout from './Layout';
-/*import Home from '../pages/Home';
+import Home from '../pages/Home';
+
+/*
 import Badges from '../pages/Badges';
 import BadgeNew from '../pages/BadgeNew';
 import BadgeDetails from '../pages/BadgeDetailsContainer';
@@ -10,8 +12,6 @@ import BadgeEdit from '../pages/BadgeEdit';
 import NotFound from '../pages/NotFound';*/
 
 /*
-
-          <Route exact path="/" component={Home} />
           <Route exact path="/badges" component={Badges} />
           <Route exact path="/badges/new" component={BadgeNew} />
           <Route exact path="/badges/:badgeId" component={BadgeDetails} />
@@ -24,6 +24,7 @@ function App() {
     <BrowserRouter>
       <Layout>
         <Switch>
+        <Route exact path="/" component={Home} />
         </Switch>
       </Layout>
     </BrowserRouter>
